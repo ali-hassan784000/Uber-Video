b7befad04a22d683c4b6441bf902423613345924
Ali Hassan <alihassan032586@gmail.com>
2024-12-02 12:37:38 -0800

    initial commit for uber-video project

---
+++ app.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+module.exports = app;
